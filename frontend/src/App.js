98336e2df6e75dfee8394fc16a1038435922a1ac
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-09 09:49:24 +0530

    2.0

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
