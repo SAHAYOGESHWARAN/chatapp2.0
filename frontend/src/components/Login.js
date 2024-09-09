5aa2ccfe190bbfcb998a546c5d9ec31bb851e1e0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-09 09:54:07 +0530

    2.0

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,14 +19,21 @@ const Login = () => {
 
   return (
     <div>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e)
-        setPassword(e.target.value)}
-         />
-         <button onClick={handleLogin}>Login</button>
-       </div>
-     );
-   };
-  
-   export default Login;
-  
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
